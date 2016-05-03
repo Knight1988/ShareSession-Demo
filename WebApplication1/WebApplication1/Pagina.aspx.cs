468a6cceee75a8d857d7846c2ddde048df0d0fd6
Pablo Degradi <pdegradi@gmail.com>
2016-05-03 16:26:49 +0200

    first commit

---
+++ WebApplication1/WebApplication1/Pagina.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class Pagina : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            miodiv.InnerHtml = HttpContext.Current.Session["miooggetto"] == null ? "NO SESSION" : HttpContext.Current.Session["miooggetto"].ToString();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication1/Pagina.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pagina.aspx.cs" Inherits="WebApplication1.Pagina" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div id="miodiv" runat="server"></div>
+    </form>
+</body>
+</html>
